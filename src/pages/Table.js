e94b17cd9d5db4d5ed0f6c808686a9dbc995a255
leong_jia_wei <wwwbingo6@gmail.com>
2022-12-21 23:36:59 +0800

    commit

---
+++ src/pages/Table.js
@@ -0,0 +1,89 @@
+import React from "react";
+import styled from "styled-components";
+import { observer } from "mobx-react";
+import store from "./store";
+
+const Table = styled.table`
+  margin-top: 20px;
+  width: 100%;
+
+  td,
+  th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+  }
+
+  tr:nth-child(even) {
+    background-color: #dddddd;
+  }
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  margin: 5px;
+
+  div {
+    border-right: 1px solid rgba(0, 0, 0, 0.2);
+  }
+`;
+
+function ListTable() {
+  console.log(store.Title, "data");
+
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Image</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <img src={store.data.Poster} alt="image" />
+          </td>
+          <td>
+            <Container>
+              <div>Title:</div>
+              {store.data.Title}
+            </Container>
+            <Container>
+              <div>Rated:</div>
+              {store.data.Rated}
+            </Container>
+            <Container>
+              <div>Rating:</div> {store.data.imdbRating}
+            </Container>
+            <Container>
+              <div>Released Date:</div> {store.data.Released}
+            </Container>
+            <Container>
+              <div>Duration:</div> {store.data.Runtime}
+            </Container>
+            <Container>
+              <div>Plot:</div> {store.data.Plot}
+            </Container>
+            <Container>
+              <div>Director:</div> {store.data.Director}
+            </Container>
+            <Container>
+              <div>Actors:</div> {store.data.Actors}
+            </Container>
+            <Container>
+              <div>Language:</div> {store.data.Language}
+            </Container>
+            <Container>
+              <div>Awards:</div> {store.data.Awards}
+            </Container>
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+export default observer(ListTable);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,69 @@
-import * as React from "react"
+import React, { useState } from "react";
+import styled from "styled-components";
+import ListTable from "./Table";
+import { observer } from "mobx-react";
+import store from "./store";
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const Form = styled.form`
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  column-gap: 10px;
+  height: 30px;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+
+  & > input {
+    border-radius: 5px;
+    height: 100%;
+    border: 1px solid black;
+  }
+
+  & > button {
+    height: 100%;
+    border-radius: 5px;
+    border: 1px solid black;
+    background: none;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+      background: black;
+      color: white;
+    }
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const Home = () => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    store.getData(value);
+  };
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <input
+          placeholder="Keyword"
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
+        <button>Submit</button>
+      </Form>
+      {store.data.Title ? <ListTable /> : <div>No Data</div>}
+    </Container>
+  );
+};
+
+export default observer(Home);
